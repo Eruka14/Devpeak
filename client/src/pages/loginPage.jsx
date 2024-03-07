b30f077490d95d634e3a4fe0852406f160c77948
Fyodor <shobshob481@gmail.com>
2024-03-07 18:31:24 +0800

    Full version of register

--- client/src/pages/loginPage.jsx
+++ client/src/pages/loginPage.jsx
@@ -4,11 +4,14 @@ import usePasswordToggle from "../hooks/usePasswordToggle";
 
 const LoginPage = () => {
   const [PasswordInputType, ToggleIcon] = usePasswordToggle();
+  // Алдааны мэдээлэл хадгалах төлөв
+  const [Err, setErr] = useState(null);
+  // Хэрэглэгчийн оруулсан мэдээллийг хадгалах төлөв
   const [Inputs, setInputs] = useState({
     email: "",
     password: "",
   });
-
+  // Хэрэглэгчийн оруулсан мэдээллийг төлөврүү дамжуулах
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
@@ -16,9 +19,11 @@ const LoginPage = () => {
     <div className="flex justify-center items-center h-screen bg-gray-200">
       <form action="">
         <div className="bg-white px-6 pt-4 pb-4 rounded-md w-[300px] drop-shadow-md">
+          {/* Гарчиг */}
           <h1 className="text-2xl text-center mb-4 font-medium text-gray-700">
             Нэвтрэх
           </h1>
+          {/* Хэрэглэгчийн и-мэйл input */}
           <div className="my-3">
             <label htmlFor="email" className="block pb-1">
               И-мэйл
@@ -31,20 +36,23 @@ const LoginPage = () => {
               className="border border-slate-300 rounded-md w-[100%] h-8 pl-2"
             />{" "}
           </div>
+          {/* Хэрэглэгчийн нууц үг оруулах хэсэг */}
           <div>
             <label htmlFor="password" className="block pb-1">
               Нууц үг
             </label>
-            <input
-              type={PasswordInputType}
-              id="password"
-              name="password"
-              onChange={handleChange}
-              className="border border-slate-300 rounded-md w-[100%] h-8 pl-2"
-            />
-            <span className="absolute bottom-24 right-8 z-50 cursor-pointer">
-              {ToggleIcon}
-            </span>
+            <div className="flex justify-end">
+              <input
+                type={PasswordInputType}
+                id="password"
+                name="password"
+                onChange={handleChange}
+                className="border border-slate-300 rounded-md w-[100%] h-8 pl-2"
+              />
+              <span className="absolute mt-2 mr-2 z-50 cursor-pointer">
+                {ToggleIcon}
+              </span>
+            </div>
           </div>
           <div className="flex justify-center">
             <button
@@ -57,6 +65,10 @@ const LoginPage = () => {
           <div className="w-[90%] pt-2 m-auto">
             <hr />
           </div>
+          {/* Алдаа харуулах хэсэг */}
+          {Err && (
+            <p className="text-center text-sm text-red-600 mt-3">{Err}</p>
+          )}
         </div>
         <div className="bg-white px-6 pt-4 pb-4 rounded-md w-[300px] drop-shadow-md mt-3">
           <p className="text-sm text-center">
--- client/src/pages/registerPage.jsx
+++ client/src/pages/registerPage.jsx
@@ -1,64 +1,90 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import usePasswordToggle from "../hooks/usePasswordToggle";
 
 const LoginPage = () => {
+  // Нууц үг нуух болон харах icon
   const [PasswordInputType, ToggleIcon] = usePasswordToggle();
+  // Хэрэглэгчийн мэдээллийг хадгалах төлөв
   const [Inputs, setInputs] = useState({
     username: "",
     email: "",
     password: "",
   });
+  // Алдааний мэдээллийг хадгалах төлөв
+  const [Err, setErr] = useState(null);
+  // Хэрэглэгчийн оруулсан мэдээллийг төлөврүү дамжуулах функц
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
+
+  // Хэрэглэгчийн оруулсан мэдээллийг өгөгдлийн санруу дамжуулах
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8000/api/auth/register", Inputs);
+    } catch (Err) {
+      setErr(Err.response.data);
+    }
+  };
+  console.log(Err);
   return (
     <div className="flex justify-center items-center h-screen bg-gray-200">
       <form action="">
         <div className="bg-white px-6 pt-4 pb-4 rounded-md w-[300px] drop-shadow-md">
+          {/* Гарчиг */}
           <h1 className="text-2xl text-center mb-4 font-medium text-gray-700">
             Бүртгүүлэх
           </h1>
+          {/* Хэрэглэгчийн нэрний input */}
           <div className="my-3">
             <label htmlFor="username" className="block pb-1">
               Нэр
             </label>
             <input
-              type="username"
-              id="username"
+              type="text"
+              name="username"
               onChange={handleChange}
               className="border border-slate-300 rounded-md w-[100%] h-8 pl-2"
             />{" "}
           </div>
+          {/* Хэрэглэгчийн и-мэйл input */}
           <div className="my-3">
             <label htmlFor="email" className="block pb-1">
               И-мэйл
             </label>
             <input
               type="email"
-              id="email"
+              name="email"
               onChange={handleChange}
               className="border border-slate-300 rounded-md w-[100%] h-8 pl-2"
             />{" "}
           </div>
+          {/* Хэрэглэгчийн нууц үг input */}
           <div>
             <label htmlFor="password" className="block pb-1">
               Нууц үг
             </label>
-            <input
-              type={PasswordInputType}
-              id="password"
-              onChange={handleChange}
-              className="border border-slate-300 rounded-md w-[100%] h-8 pl-2"
-            />
-            <span className="absolute bottom-24 right-8 z-50 cursor-pointer">
-              {ToggleIcon}
-            </span>
+            <div className="flex justify-end">
+              <input
+                type={PasswordInputType}
+                name="password"
+                onChange={handleChange}
+                className="border border-slate-300 rounded-md w-[100%] h-8 pl-2"
+              />
+              {/* hide / reveal icon button */}
+              <span className="absolute z-50 cursor-pointer mt-2 mr-2">
+                {ToggleIcon}
+              </span>
+            </div>
           </div>
+
           <div className="flex justify-center">
             <button
               type="submit"
               className="block w-[95%] bg-green-600 rounded-md py-1 mt-6 font-bold text-white mb-2 hover:bg-green-500"
+              onClick={handleClick}
             >
               Бүртгүүлэх
             </button>
@@ -66,6 +92,10 @@ const LoginPage = () => {
           <div className="w-[90%] pt-2 m-auto">
             <hr />
           </div>
+          {/* back end-с илгээсэн алдааг харуулах */}
+          {Err && (
+            <p className="text-center text-sm text-red-600 mt-3">{Err}</p>
+          )}
         </div>
       </form>
     </div>
