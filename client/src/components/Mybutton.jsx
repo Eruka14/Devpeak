e16655ff52b74d8ed8da91a0b2b3466c8507ec9b
Fyodor <shobshob481@gmail.com>
2024-03-20 01:31:03 +0800

    indexpage 80%

--- client/src/components/Mybutton.jsx
+++ client/src/components/Mybutton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Mybutton = (props) => {
   return (
     <button
-      className={`border-[1px] border-slate-300 rounded-md py-2 px-4 m-2 text-sm font-semibold ${props.style} text-xs`}
+      className={`border-[1px] border-slate-300 rounded-md py-2 px-4 m-2 font-semibold text-xs ${props.style}`}
     >
       {props.children}
     </button>
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,38 +1,69 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Mybutton from "../components/Mybutton";
+import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
+import { FcAbout } from "react-icons/fc";
+import { ReactTyped } from "react-typed";
 
 const indexPage = () => {
   return (
     <>
       <Navbar />
-      <div className="grid text-black-800 w-[1280px] h-[95vh] items-center justify-center m-auto">
-        <div className="">
-          <h1 className="text-6xl font-bold text-center text-slate-700">
-            <span className="bg-gradient-to-r from-purple-700 via-blue-700 to-pink-500 text-transparent bg-clip-text text-6xl font-bold">
+      <div className="grid text-black-800 w-[1280px] h-[80vh] items-center justify-center m-auto">
+        <div>
+          <h1 className="text-8xl font-bold text-center text-slate-700">
+            <span className="bg-gradient-to-r from-purple-700 via-blue-700 to-pink-500 text-transparent bg-clip-text text-8xl font-bold">
               {" "}
               Devpeak{" "}
             </span>
-            тавтай морил
+            <ReactTyped
+              strings={["тавтай морил"]}
+              typeSpeed={130}
+              backSpeed={70}
+              loop
+            />
           </h1>
-          <h2 className="text-2xl text-center pt-5 text-slate-700 font-semibold">
-            Tанд асуух асуулт байвал бүртгүүлэх хэсэг дээр дарж цааш
-            үргэлжлүүлнэ үү!!!
+          <h2 className="text-3xl text-center pt-7 text-slate-700 font-medium">
+            Программчлалын асуулт, хариултын веб платформ
           </h2>
-          <div className="text-center pt-5">
-            <Mybutton style="items-center text-[1.2rem] py-2 pb-[0.7rem] shadow-xl ease-in-out hover:bg-blue-950 hover:text-white duration-300">
-              Эхлэх &rarr;
-            </Mybutton>
+          <div className="flex justify-center mt-10">
+            <Link to="/login">
+              <Mybutton style="flex items-center py-2 px-6 ease-in-out hover:bg-blue-950 hover:text-white duration-300 font-extrabold">
+                Эхлэх &rarr;
+              </Mybutton>
+            </Link>
           </div>
         </div>
-        <div className="w-[900px] bg-slate-700 text-white rounded-md px-10 py-5">
-          <h1 className="">Зорилго</h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius aliquam
-          voluptatibus laboriosam! Ab deleniti explicabo velit optio quia sunt,
-          dolor consequuntur molestiae itaque ut placeat quisquam vitae aperiam
-          possimus eum.
+      </div>
+      <div className="w-full bg-gradient-to-r from-slate-800  to-blue-900 text-white h-[50vh]">
+        <div className="w-[80%] flex justify-between items-center mx-auto h-[100%]">
+          <div className="w-[40%] grid gap-10">
+            <h2 className="text-2xl font-semibold text-slate-200 flex items-center">
+              <FcAbout />
+              <span className="ml-2">Веб сайтын тухай</span>
+            </h2>
+            <p className="text-justify text-slate-200">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+              amet eius velit, explicabo, officiis quas cumque necessitatibus
+              dicta ea tempore quia ad. Ab hic reprehenderit odio? Eaque
+              recusandae sunt hic?
+            </p>
+          </div>
+          <div className="w-[40%] grid gap-10">
+            <h2 className="text-2xl font-semibold text-slate-200">Мэдэхгүй</h2>
+            <p className="text-justify text-slate-200">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+              amet eius velit, explicabo, officiis quas cumque necessitatibus
+              dicta ea tempore quia ad. Ab hic reprehenderit odio? Eaque
+              recusandae sunt hic?
+            </p>
+          </div>
         </div>
       </div>
+
+      <div className="w-full h-[50vh]"></div>
+      <Footer />
     </>
   );
 };
